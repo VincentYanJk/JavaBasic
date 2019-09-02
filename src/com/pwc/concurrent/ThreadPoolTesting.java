23bcba3a6ae5041fa1a2de76f21a1a99b83311b6
jyan062 <jyan062@jyan06204.us.nam.ad.pwcinternal.com>
2019-09-02 16:18:16 +0800

    jason yan - update a little

--- src/com/pwc/concurrent/ThreadPoolTesting.java
+++ src/com/pwc/concurrent/ThreadPoolTesting.java
@@ -6,24 +6,31 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
 public class ThreadPoolTesting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(5, 10, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(5));
-		//ExecutorService threadPool = Executors.newFixedThreadPool(5);
+		ExecutorService threadPool = Executors.newFixedThreadPool(5);
 		
 		for (int i = 0; i < 20; i++) {
 			
 			MyTask task = new MyTask(i);
 			
 			poolExecutor.execute(task);
+			poolExecutor.execute(()->{
+				System.out.println("");
+				
+			} );
+			
+		
+			
             System.out.println("There are ："+poolExecutor.getPoolSize()+" in the Thread pool"+"，There are ："+
             		poolExecutor.getQueue().size()+" in the queue"+"，there are ："+poolExecutor.getCompletedTaskCount()+" finished");
-			
-			
-			
+
 		}
 
 	}
